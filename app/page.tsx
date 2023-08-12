d9752b0adceb5f8160cefd8f53114846f34a0036
themuuln <zerone.offical@gmail.com>
2023-08-12 14:26:08 +0800

    Numerology Calculator added

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,111 @@
-import Image from 'next/image'
+"use client";
+import { useState } from "react";
 
-export default function Home() {
+type Props = {};
+
+type State = {
+  dateOfBirth: string;
+  fullName: string;
+  lifePathNumber: number | null;
+  destinyNumber: number | null;
+};
+
+export default function Home(_props: Props): JSX.Element {
+  const [state, setState] = useState<State>({
+    dateOfBirth: "",
+    fullName: "",
+    lifePathNumber: null,
+    destinyNumber: null,
+  });
+
+  const calculateLifePathNumber = (): void => {
+    const { dateOfBirth, fullName } = state;
+
+    const cleanedDateOfBirth = dateOfBirth.replace(/\D/g, "");
+
+    let sum = 0;
+    for (let i = 0; i < cleanedDateOfBirth.length; i++) {
+      sum += parseInt(cleanedDateOfBirth[i]);
+    }
+
+    while (sum > 9 && sum !== 11 && sum !== 22 && sum !== 33) {
+      sum = Math.floor(sum / 10) + (sum % 10);
+    }
+
+    setState((prevState) => ({
+      ...prevState,
+      lifePathNumber: sum,
+    }));
+
+    calculateDestinyNumber(fullName);
+  };
+
+  const calculateDestinyNumber = (fullName: string): void => {
+    const cleanedFullName = fullName.replace(/\s/g, "").toUpperCase();
+
+    let sum = 0;
+    for (let i = 0; i < cleanedFullName.length; i++) {
+      const charCode = cleanedFullName.charCodeAt(i);
+      if (charCode >= 65 && charCode <= 90) {
+        sum += charCode - 64;
+      }
+    }
+
+    while (sum > 9 && sum !== 11 && sum !== 22 && sum !== 33) {
+      sum = Math.floor(sum / 10) + (sum % 10);
+    }
+
+    setState((prevState) => ({
+      ...prevState,
+      destinyNumber: sum,
+    }));
+  };
+
+  const { dateOfBirth, fullName, lifePathNumber, destinyNumber } = state;
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
+    <div className='flex flex-col items-center'>
+      <h1 className='text-3xl font-bold mb-4'>
+        Numerology Life Path Number Calculator
+      </h1>
+      <label className='text-lg'>Date of Birth:</label>
+      <input
+        type='text'
+        value={dateOfBirth}
+        onChange={(e) =>
+          setState((prevState) => ({
+            ...prevState,
+            dateOfBirth: e.target.value,
+          }))
+        }
+        className='border border-gray-300 px-4 py-2 rounded-md w-64 mb-4'
+      />
+      <label className='text-lg'>Full Name:</label>
+      <input
+        type='text'
+        value={fullName}
+        onChange={(e) =>
+          setState((prevState) => ({
+            ...prevState,
+            fullName: e.target.value,
+          }))
+        }
+        className='border border-gray-300 px-4 py-2 rounded-md w-64 mb-4'
+      />
+      <button
+        onClick={calculateLifePathNumber}
+        className='bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded'
+      >
+        Calculate
+      </button>
+      {lifePathNumber !== null && (
+        <p className='text-lg mt-4'>
+          Your Life Path Number is: {lifePathNumber}
         </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+      )}
+      {destinyNumber !== null && (
+        <p className='text-lg mt-2'>Your Destiny Number is: {destinyNumber}</p>
+      )}
+    </div>
+  );
 }
